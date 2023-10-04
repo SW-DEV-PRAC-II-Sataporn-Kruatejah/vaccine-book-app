ca08e348fde09263cf2f039512a3068da4f15ecd
saimkrua <k.satapornimp@gmail.com>
2023-10-04 16:12:52 +0700

    [ADD] assignment8
    Assignment 8: React Hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import React from 'react';
 import Banner from '@/components/Banner';
+import { PromoteCard } from '@/components/PromoteCard';
 export default function Home() {
   return (
     <main>
       <Banner />
+      <PromoteCard />
     </main>
   );
 }
---
+++ src/components/PromoteCard.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { VideoPlayer } from "./VideoPlayer"
+import { useState } from "react"
+import { useWindowListener } from "@/hooks/useWindowListener"
+
+export function PromoteCard() {
+    const [playing, setPlaying] = useState(true)
+    useWindowListener("contextmenu", (e) => { e.preventDefault() })
+
+    return (
+        <div className="w-[80%] shadow-lg mx-[10%] my-10 p-2 rounded-lg bg-gray-200 flex flex-row">
+            <VideoPlayer isPlaying={playing} vdoSrc="/video/getvaccine.mp4"></VideoPlayer>
+            <div className="m-5 flex flex-col w-full items-center justify-center">
+                Get your vaccine today.
+                <button
+                    className="w-20 block rounded-md bg-blue-600 hover:bg-blue-800 px-3 py-2 text-white shadow-sm"
+                    onClick={() => setPlaying(!playing)}>
+                    {playing ? "Pause" : "Play"}
+                </button>
+            </div>
+        </div>
+    )
+}
